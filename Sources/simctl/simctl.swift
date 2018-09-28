d80ea572f3ad69c2f7ac2657107b442278b5c91f
Pedro Piñera <pepibumur@gmail.com>
2018-09-28 12:56:30 +0200

    Generate documentation

---
+++ Sources/simctl/simctl.swift
@@ -0,0 +1,12 @@
+import TuistCore
+
+/// Simctl device.
+public class Device {
+    
+    /// Device name.
+    public let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
