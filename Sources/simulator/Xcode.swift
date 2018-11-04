25fafcedf4607753d5750e5cfc57b4b767db49e4
Pedro Piñera <pepibumur@gmail.com>
2018-11-04 15:31:58 +0100

    Add Xcode.runtimeProfilesPath

--- Sources/simulator/Xcode.swift
+++ Sources/simulator/Xcode.swift
@@ -26,13 +26,26 @@ struct Xcode: Xcoding {
         self.shell = shell
     }
 
+    /// Returns a signal producer that returns the path where the platform simulator runtimes are located.
+    ///
+    /// - Parameter platform: Platform whose runtime profiles will be returned.
+    /// - Returns: Signal producer that returns the path.
+    func runtimeProfilesPath(platform: Runtime.Platform) -> SignalProducer<URL?, ShellError> {
+        guard let device = devicePlatform(platform: platform) else {
+            return SignalProducer(value: nil)
+        }
+        return shell.xcodePath()
+            .map({ URL(fileURLWithPath: $0, isDirectory: true) })
+            .map({ $0.appendingPathComponent("Platforms/\(device).platform/Developer/Library/CoreSimulator/Profiles/Runtimes/") })
+    }
+
     /// Returns a signal producer that returns the path where the platform simulator
     ///
     /// - Parameters:
     ///   - platform: Platform whose simulator SDK path will be returned
     /// - Returns: Signal producer that returns the path where the platform simulator SDK is located.
     func simulatorSDKPath(platform: Runtime.Platform) -> SignalProducer<URL?, ShellError> {
-        guard let simulator = simulator(platform: platform) else {
+        guard let simulator = simulatorPlatform(platform: platform) else {
             return SignalProducer(value: nil)
         }
         return shell.xcodePath()
@@ -40,11 +53,11 @@ struct Xcode: Xcoding {
             .map({ $0.appendingPathComponent("Platforms/\(simulator).platform/Developer/SDKs/\(simulator).sdk/") })
     }
 
-    /// Given a platform, it returns the name of the simulator platform.
+    /// Given a platform, it returns the name of the simulator platform to look it up in the Developer/Platforms directory.
     ///
     /// - Parameter platform: Platform.
     /// - Returns: Simulator platform (e.g. iPhoneSimulator.platform)
-    func simulator(platform: Runtime.Platform) -> String? {
+    func simulatorPlatform(platform: Runtime.Platform) -> String? {
         switch platform {
         case .iOS:
             return "iPhoneSimulator"
@@ -52,6 +65,23 @@ struct Xcode: Xcoding {
             return "WatchSimulator"
         case .tvOS:
             return "AppleTVSimulator"
+        default:
+            return nil
+        }
+    }
+
+    /// Given a platform, it returns the device name to look it up in the Developer/Platforms directory.
+    ///
+    /// - Parameter platform: Platform.
+    /// - Returns: Device platform name.
+    func devicePlatform(platform: Runtime.Platform) -> String? {
+        switch platform {
+        case .iOS:
+            return "iPhoneOS"
+        case .watchOS:
+            return "WatchOS"
+        case .tvOS:
+            return "AppleTVOS"
         default:
             return nil
         }
--- Tests/SimulatorTests/XcodeTests.swift
+++ Tests/SimulatorTests/XcodeTests.swift
@@ -13,6 +13,27 @@ final class XcodeTests: XCTestCase {
         subject = Xcode(shell: shell)
     }
 
+    func test_runtimeProfilesPath() throws {
+        shell.xcodePathStub = {
+            .success("/xcode")
+        }
+        guard let got = subject.runtimeProfilesPath(platform: .iOS).single() else {
+            XCTFail("Expected simulatorSDKPath to return a value")
+            return
+        }
+        let value = try XCTTry(try got.dematerialize())
+        XCTAssertEqual(value, URL(fileURLWithPath: "/xcode/Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes"))
+    }
+
+    func test_runtimeProfilesPath_when_platformHasNoSimulator() throws {
+        guard let got = subject.runtimeProfilesPath(platform: .unknown).single() else {
+            XCTFail("Expected simulatorSDKPath to return a value")
+            return
+        }
+        let value = try XCTTry(try got.dematerialize())
+        XCTAssertNil(value)
+    }
+
     func test_simulatorSDKPath() throws {
         shell.xcodePathStub = {
             .success("/xcode")
@@ -34,9 +55,15 @@ final class XcodeTests: XCTestCase {
         XCTAssertNil(value)
     }
 
-    func test_simulator() {
-        XCTAssertEqual(subject.simulator(platform: .iOS), "iPhoneSimulator")
-        XCTAssertEqual(subject.simulator(platform: .watchOS), "WatchSimulator")
-        XCTAssertEqual(subject.simulator(platform: .tvOS), "AppleTVSimulator")
+    func test_devicePlatform() throws {
+        XCTAssertEqual(subject.devicePlatform(platform: .iOS), "iPhoneOS")
+        XCTAssertEqual(subject.devicePlatform(platform: .watchOS), "WatchOS")
+        XCTAssertEqual(subject.devicePlatform(platform: .tvOS), "AppleTVOS")
+    }
+
+    func test_simulatorPlatform() {
+        XCTAssertEqual(subject.simulatorPlatform(platform: .iOS), "iPhoneSimulator")
+        XCTAssertEqual(subject.simulatorPlatform(platform: .watchOS), "WatchSimulator")
+        XCTAssertEqual(subject.simulatorPlatform(platform: .tvOS), "AppleTVSimulator")
     }
 }
